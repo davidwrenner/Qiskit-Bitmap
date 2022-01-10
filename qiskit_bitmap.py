74b717cced93022e4ffb09b19d4d5c83ae3babc0
David Wrenner <davidwrenner@wustl.edu>
2022-01-09 22:25:39 -0500

    v0.1

--- qiskit_bitmap.py
+++ qiskit_bitmap.py
@@ -1,14 +1,16 @@
+#!/usr/bin/env python3
 
 import random
 import sys
 from PIL import Image, ImageFont, ImageDraw
 from qiskit import QuantumCircuit
 
+EXPECTED_ARG_COUNT = 2
 DPI = 250
 FONT_SIZE = 19
 FONT_FILE = 'Times.ttc'
 OUTPUT_MODE = 'mpl'
-OUTPUT_FILE = './qiskit_text.png'
+OUTPUT_FILE = 'qiskit_text.png'
 MAX_TEXT_LENGTH = 200
 H_COLOR = '#33B1FF'
 I_COLOR = '#F0F0F0'
@@ -19,6 +21,25 @@ Z_COLOR = '#F0F0F0'
 
 def usage():
     print(f'Usage: python {sys.argv[0]} <textstring>\n')
+
+
+def get_font():
+    return ImageFont.truetype(FONT_FILE, FONT_SIZE)
+
+
+def get_text_size(text):
+    return get_font().getsize(text)
+
+
+def text_to_image(text):
+    print('Generating text...')
+    w, h = get_text_size(text)
+    image = Image.new('1', (w, h), 1)
+    draw = ImageDraw.Draw(image)
+    font = get_font()
+    draw.text((0, 0), text, font=font)
+    return image
+
 
 def append_random(qc, qubit):
     n = random.uniform(0, 1)
@@ -41,24 +62,8 @@ def bit_to_gate(qc, img, c, r):
     return qc
 
 
-def get_font():
-    return ImageFont.truetype(FONT_FILE, FONT_SIZE)
-
-
-def get_text_size(text):
-    return get_font().getsize(text)
-
-
-def text_to_image(text):
-    w, h = get_text_size(text)
-    image = Image.new('1', (w, h), 1)
-    draw = ImageDraw.Draw(image)
-    font = get_font()
-    draw.text((0, 0), text, font=font)
-    return image
-
-
 def image_to_circuit(img, text):
+    print('Generating circuit...')
     w, h = get_text_size(text)
     qc = QuantumCircuit(h-2,1)
     for r in range(2,h):
@@ -67,19 +72,22 @@ def image_to_circuit(img, text):
     return qc
 
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
+def main():
+    if len(sys.argv) != EXPECTED_ARG_COUNT:
         usage()
-        sys.exit(0)
+        return
     text = sys.argv[1]
     if len(text) > MAX_TEXT_LENGTH:
-        print(f'Exceeded maximum text length of {MAX_TEXT_LENGTH}.')
-        sys.exit(0)
-    image = text_to_image(text)
-    print('Building circuit...')
+        print(f'Length of input exceeded max. \
+            Actual: {len(text)}, Expected: {MAX_TEXT_LENGTH}')
+        return
+    try:
+        image = text_to_image(text)
+    except OSError:
+        print(f'Unable to locate {FONT_FILE}')
+        return
     qc = image_to_circuit(image, text)
-    fold = MAX_TEXT_LENGTH
-    print('Generating image...')
+    print('Drawing circuit...')
     qc.draw(output=OUTPUT_MODE,
         filename=OUTPUT_FILE,
         fold=MAX_TEXT_LENGTH,
@@ -94,4 +102,8 @@ if __name__ == '__main__':
             'dpi': DPI
         }
     )    
-    print(f'Image generated, view output at {OUTPUT_FILE}')
+    print(f'Finished. Output at {OUTPUT_FILE}')
+
+
+if __name__ == '__main__':
+    main()
